af15a5b193aee1248c24386a514234ef76565547
Tiago Proenca <tproenca@gmail.com>
2019-02-22 22:38:37 -0800

    Fixed download JS script

--- plexserverurl.js
+++ plexserverurl.js
@@ -7,14 +7,28 @@ phantomjs.run('--webdriver=4444').then(program => {
   webdriverio
   .remote()
   .init()
-  .url('https://www.plex.tv/downloads')
-  .click('#block-pms')
+  .url('https://www.plex.tv/media-server-downloads')
   .waitForVisible('.plex-downloads-pms-platform', 5000)
   .selectByVisibleText('.plex-downloads-pms-platform', 'Synology')
-  .element('//div[contains(@class, "plex-downloads-pms-releases")]//a[text()="ARMv7"]')
+  .click('//button[text()="Choose Package"]')
+  .waitForVisible('.tooltipster-content', 5000)
+  .element('//div[contains(@class, "tooltipster-content")]//a[text()="ARMv7"]')
+  .getAttribute('href')
+  .then(function(href) {
+    console.log(href);
+  })
+  .url('https://www.plex.tv/media-server-downloads')
+  .waitForVisible('.plex-downloads-pms-platform', 5000)
+  .selectByVisibleText('.plex-downloads-pms-platform', 'Netgear')
+  .click('//button[text()="Choose Package"]')
+  .waitForVisible('.tooltipster-content', 5000)
+  .element('//div[contains(@class, "tooltipster-content")]//a[text()="ARM 6.x RN2xx"]')
   .getAttribute('href')
   .then(function(href) {
      console.log(href);
+  })
+  .catch(function(err) {
+    console.log(err);
   })
   .end()
   .then(function(){
