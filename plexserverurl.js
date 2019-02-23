f7b338b27e0d2da4ed98220d0277083db4d77328
Tiago Proenca <tproenca@gmail.com>
2019-02-22 23:22:00 -0800

    Removed ARM 6.x RN2xx

--- plexserverurl.js
+++ plexserverurl.js
@@ -17,16 +17,6 @@ phantomjs.run('--webdriver=4444').then(program => {
   .then(function(href) {
     console.log(href);
   })
-  .url('https://www.plex.tv/media-server-downloads')
-  .waitForVisible('.plex-downloads-pms-platform', 5000)
-  .selectByVisibleText('.plex-downloads-pms-platform', 'Netgear')
-  .click('//button[text()="Choose Package"]')
-  .waitForVisible('.tooltipster-content', 5000)
-  .element('//div[contains(@class, "tooltipster-content")]//a[text()="ARM 6.x RN2xx"]')
-  .getAttribute('href')
-  .then(function(href) {
-     console.log(href);
-  })
   .catch(function(err) {
     console.log(err);
   })
